8fd37cd2cc9e8eb72786f6ac155623c03d6d19c0
jero98772 <jero98772@gmail.com>
2024-10-15 11:51:58 -0500

    update

--- server/test2.c
+++ server/test2.c
@@ -13,9 +13,9 @@
 #define MAX_CLIENTS 100
 #define IP_POOL_START "192.168.1.100"
 #define IP_POOL_END "192.168.1.200"
-#define SERVER_IP "127.0.0.1"
-#define DHCP_SERVER_PORT 6767
-#define DHCP_CLIENT_PORT 6868
+#define SERVER_IP "0.0.0.0"
+#define DHCP_SERVER_PORT 6761
+#define DHCP_CLIENT_PORT 6862
 #define MAX_DHCP_PACKET_SIZE 1024
 #define LOG_FILE "dhcp_server.log"
 #define CONFIG_FILE "dhcp_config.txt"
@@ -238,9 +238,12 @@ void handle_dhcp_discover(DHCPPacket *packet, struct sockaddr_in *client_addr) {
     server_addr.sin_port = htons(dhcp_server_port);
 
     if (bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        char error_msg[256];
+        snprintf(error_msg, sizeof(error_msg), "Bind failed: %s", strerror(errno));
+        write_log(error_msg);
         perror("Bind failed");
         close(sock);
-        return;
+        return NULL;
     }
 
     client_addr->sin_port = htons(dhcp_client_port);
@@ -327,9 +330,12 @@ void handle_dhcp_request(DHCPPacket *packet, struct sockaddr_in *client_addr) {
     server_addr.sin_port = htons(dhcp_server_port);
 
     if (bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        char error_msg[256];
+        snprintf(error_msg, sizeof(error_msg), "Bind failed: %s", strerror(errno));
+        write_log(error_msg);
         perror("Bind failed");
         close(sock);
-        return;
+        return NULL;
     }
 
 client_addr->sin_port = htons(dhcp_client_port);
@@ -505,6 +511,9 @@ void* dhcp_server_thread(void* arg) {
     server_addr.sin_port = htons(dhcp_server_port);
 
     if (bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        char error_msg[256];
+        snprintf(error_msg, sizeof(error_msg), "Bind failed: %s", strerror(errno));
+        write_log(error_msg);
         perror("Bind failed");
         close(sock);
         return NULL;
